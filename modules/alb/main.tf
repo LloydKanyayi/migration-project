2992f0dc6b4836be7f43dbcd15189ce508dc905f
Lloyd Kanyayi <lloyd.kanyayi@wrenkitchens.com>
2021-03-09 14:35:14 +0000

    commit updates from alb module

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -1,7 +1,9 @@
 # # --------------alb/main.tf.tf
 
-resource "aws_lb" "test" {
-  name               = "test-lb-tf"
+
+
+resource "aws_alb" "main" {
+  name               = "myapp-target-group"
   internal           = false
   load_balancer_type = "application"
   security_groups    = var.security_groups
@@ -11,5 +13,61 @@ resource "aws_lb" "test" {
 
   tags = {
     Name = "main-alb"
+  }
+}
+
+
+resource "aws_lb_target_group" "app" {
+    name        = "myapp-target-group"
+    port        = 80
+    protocol    = "HTTP"
+    target_type = "ip"
+    vpc_id      = var.aws_vpc
+
+  health_check {
+    healthy_threshold   = "3"
+    interval            = "30"
+    protocol            = "HTTP"
+    matcher             = "200"
+    timeout             = "3"
+    path                = "/"
+    unhealthy_threshold = "2"
+  }
+}
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_alb.main.id
+  port              = "80"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.id
+  }
+}
+
+
+# ALB Security Group: Edit to restrict access to the application
+
+resource "aws_security_group" "lb" {
+  description = "Allow inbound traffic"
+  vpc_id      = var.aws_vpc
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = var.app_port
+    to_port     = var.app_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "lb"
   }
 }
--- modules/alb/variables.tf
+++ modules/alb/variables.tf
@@ -1,8 +1,22 @@
 #------------------alb/variables.tf
 
+
+
 variable "security_groups" {
   default = ""
 }
+
+
 variable "public_subnets" {
+  default = ""
+}
+
+
+variable "aws_vpc" {
+  default = ""
+}
+
+
+variable "app_port" {
   default = ""
 }
