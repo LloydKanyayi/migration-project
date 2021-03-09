84e121c0141157d9a1a6fd93c5195e287a63960c
Lloyd Kanyayi <lloyd.kanyayi@wrenkitchens.com>
2021-03-09 14:38:07 +0000

    commit updates from alb module

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -1,28 +1,60 @@
-# # --------------alb/main.tf.tf
-
+# --------------alb/main.tf.tf
 
 
 resource "aws_alb" "main" {
-  name               = "myapp-target-group"
-  internal           = false
+  name            = "myapp-load-balancer"
+  subnets         = var.public_subnets
+  security_groups = var.security_groups
   load_balancer_type = "application"
-  security_groups    = var.security_groups
-  subnets            = var.public_subnets
-
-  enable_deletion_protection = false
 
   tags = {
     Name = "main-alb"
   }
 }
 
+# Redirect all traffic from the ALB to the target group
 
-resource "aws_lb_target_group" "app" {
-    name        = "myapp-target-group"
-    port        = 80
-    protocol    = "HTTP"
-    target_type = "ip"
-    vpc_id      = var.aws_vpc
+resource "aws_alb_listener" "front_end" {
+  load_balancer_arn = aws_alb.main.id
+  port              = var.app_port
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = aws_alb_target_group.app.id
+    type             = "forward"
+  }
+}
+
+
+# ALB Security Group: Edit to restrict access to the application
+resource "aws_security_group" "lb" {
+  name        = "myapp-load-balancer-security-group"
+  description = "controls access to the ALB"
+  vpc_id      = var.aws_vpc
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = var.app_port
+    to_port     = var.app_port
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+
+resource "aws_alb_target_group" "app" {
+  name        = "myapp-target-group"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = var.aws_vpc
+  target_type = "ip"
 
   health_check {
     healthy_threshold   = "3"
@@ -35,39 +67,3 @@ resource "aws_lb_target_group" "app" {
   }
 }
 
-resource "aws_lb_listener" "front_end" {
-  load_balancer_arn = aws_alb.main.id
-  port              = "80"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.app.id
-  }
-}
-
-
-# ALB Security Group: Edit to restrict access to the application
-
-resource "aws_security_group" "lb" {
-  description = "Allow inbound traffic"
-  vpc_id      = var.aws_vpc
-
-  ingress {
-    description = "TLS from VPC"
-    from_port   = var.app_port
-    to_port     = var.app_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "lb"
-  }
-}
